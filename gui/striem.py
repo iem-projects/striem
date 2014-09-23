f9957a2f1b031b59b6f550941362ede86bcce5ad
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-23 13:12:44 +0200

    show main window

---
+++ gui/striem.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014, IOhannes m zmölnig, IEM
+
+# This file is part of striem
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with striem.  If not, see <http://www.gnu.org/licenses/>.
+
+from PySide import QtGui
+import striem_ui
+
+class striem(QtGui.QMainWindow, striem_ui.Ui_striem):
+    def __init__(self, guiparent=None):
+        super(striem, self).__init__(guiparent)
+        self.setupUi(self)
+
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    window = striem()
+    window.show()
+    # Run the main Qt loop
+    sys.exit(app.exec_())
