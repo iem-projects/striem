7de789dd72c6bcb7a6f29819cbd347748ef2c863
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-23 16:00:28 +0200

    initialize gui module

---
+++ gui/__init__.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2014, IOhannes m zmölnig, IEM
+
+# This file is part of striem
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with striem.  If not, see <http://www.gnu.org/licenses/>.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__version__ = "0"
+__author__ = "IOhannes m zmölnig, IEM"
+__license__ = "GNU General Public License"
+__all__ = ["striem"]
+
+from striem import striem
