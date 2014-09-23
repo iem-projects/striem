eea20bdeadf20076c470e4203815ae7a42231090
IOhannes m zmölnig <zmoelnig@iem.at>
2014-09-23 15:17:51 +0200

    show prefs and some connections

--- gui/striem.py
+++ gui/striem.py
@@ -19,21 +19,30 @@
 # along with striem.  If not, see <http://www.gnu.org/licenses/>.
 
 from PySide import QtGui
-import streamcontrols
+import streamcontrols, streampreferences
 
 import striem_ui
 
 class striem(QtGui.QMainWindow, striem_ui.Ui_striem):
     def __init__(self):
         super(striem, self).__init__()
+        self.streamcontrol = streamcontrols.streamcontrols()
+        self.streamprefs = streampreferences.streampreferences()
+
         self.setupUi(self)
         self.setupConnections()
 
-    def setupConnectins(self):
+    def setupConnections(self):
         self.actionQuit.activated.connect(self.exit)
         self.actionStreamStart.toggled.connect(self.stream)
-        self.actionStreamControls.activated(self.open_streamcontrol)
-        self.actionStreamPrefs.activated(self.open_streamprefs)
+        self.actionStreamControls.activated.connect(self.open_streamcontrol)
+        self.actionStreamPrefs.activated.connect(self.open_streamprefs)
+        self.actionStreamPrefs.activated.connect(self.open_streamprefs)
+        self.showTextButton.clicked.connect(self.runningTick)
+        self.hideTextButton.clicked.connect(self._resetTick)
+
+    def _resetTick(self):
+        self.runningTick(False)
 
     def exit(self):
         print("Bye bye")
@@ -41,10 +50,24 @@ class striem(QtGui.QMainWindow, striem_ui.Ui_striem):
 
     def stream(self, on):
         print("stream: %s" % (on))
-    def open_streamcontrol(self);
+        self.actionStreamPrefs.setEnabled(not on)
+    def open_streamcontrol(self):
         self.streamcontrol.show()
-    def open_streamprefs(self);
+    def open_streamprefs(self):
         self.streamprefs.show()
+
+    def runningTick(self, on=True):
+        if on:
+            ## increment by one percent
+            v=self.streamstatusMeter.value()+1
+            if(v>self.streamstatusMeter.maximum()):
+                v=self.streamstatusMeter.minimum()
+                inverted=self.streamstatusMeter.invertedAppearance()
+                self.streamstatusMeter.setInvertedAppearance(not inverted)
+
+            self.streamstatusMeter.setValue(v)
+        else:
+            self.streamstatusMeter.reset()
 
 
 if __name__ == '__main__':
