e269abee2cd52069bbf8ace1cd38caf7e293da5b
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2016-10-20 13:59:47 +0200

    made 'striem' into a proper pymodule

---
+++ striem/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright © 2016, IOhannes m zmölnig, IEM
+
+# This file is part of striem - iem stream feeder
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with striem.  If not, see <http://www.gnu.org/licenses/>.
+
+from . import core
+from . import gui
+
+__version__ = "0"
+__author__ = "IOhannes m zmölnig, IEM"
+__license__ = "GNU General Public License version 2 (or later)"
+__all__ = ["core",
+           "gui",
+           ]
